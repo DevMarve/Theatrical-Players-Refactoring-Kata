0fed29e71a153d7045c8729bfad0701f7190005c
Martin Pallmet <martin.pallmert@bbv.ch>
2023-06-23 09:11:19 +0200

    Duplicate getPlay in Data record

--- java/src/main/java/theatricalplays/Data.java
+++ java/src/main/java/theatricalplays/Data.java
@@ -1,9 +1,12 @@
 package theatricalplays;
 
 public record Data(String customer, java.util.List<Performance> performances, java.util.Map<String, Play> plays) {
+	private Play getPlay(Performance perf) {
+		return this.plays.get(perf.playID);
+	}
 	public void enrichPerformances() {
 		performances.forEach(perf -> {
-			perf.setPlay(plays.get(perf.playID));
+			perf.setPlay(getPlay(perf));
 		});
 	}
 }
