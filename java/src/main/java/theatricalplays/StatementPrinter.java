d7d36acee7e576e5bd772bcba950e232742cdabe
Martin Pallmet <martin.pallmert@bbv.ch>
2023-06-23 08:59:02 +0200

    Rename variable

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -13,13 +13,13 @@ public class StatementPrinter {
         this.plays = plays;
         this.invoice = invoice;
 
-        Data data = new Data(this.invoice.customer, invoice.performances);
+        Data data = new Data(invoice.customer, invoice.performances);
         return renderPlainText(data);
     }
 
-    private String renderPlainText(Data customer) {
-        var result = String.format("Statement for %s\n", customer.customer());
-        for (var perf : customer.performances()) {
+    private String renderPlainText(Data data) {
+        var result = String.format("Statement for %s\n", data.customer());
+        for (var perf : data.performances()) {
             // print line for this order
             result += String.format("  %s: %s (%s seats)\n", getPlay(perf).name, usd(amountFor(perf)), perf.audience);
         }
