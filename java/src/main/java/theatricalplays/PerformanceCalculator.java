a5c3e6887ac3335a6c932d00fd2cf17dd7947a6b
Martin Pallmet <martin.pallmert@bbv.ch>
2023-06-23 11:13:55 +0200

    Create field Performance in PerformanceCalculator

--- java/src/main/java/theatricalplays/PerformanceCalculator.java
+++ java/src/main/java/theatricalplays/PerformanceCalculator.java
@@ -1,6 +1,9 @@
 package theatricalplays;
 
 public class PerformanceCalculator {
+	private final Performance performance;
+
 	public PerformanceCalculator(Performance aPerformance) {
+		this.performance = aPerformance;
 	}
 }
