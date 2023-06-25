825e7dd10121e13815fc2e002c344f9929b46415
marve <silentmarve@web.de>
2023-06-25 11:41:48 +0200

    Reformating and cleanup

--- java/src/main/java/theatricalplays/PerformanceCalculator.java
+++ java/src/main/java/theatricalplays/PerformanceCalculator.java
@@ -1,27 +1,18 @@
 package theatricalplays;
 
 public class PerformanceCalculator {
-	protected final Performance performance;
+    protected final Performance performance;
 
-	public PerformanceCalculator(Performance aPerformance) {
-		this.performance = aPerformance;
-	}
+    public PerformanceCalculator(Performance aPerformance) {
+        this.performance = aPerformance;
+    }
 
-	public int amount() {
-		int result;
-		switch (performance.play.type) {
-			case "tragedy":
-				throw new RuntimeException("Should be handled by subclass");
-			case "comedy":
-				throw new RuntimeException("Should be handled by subclass");
-			default:
-				throw new Error("unknown type: ${play.type}");
-		}
-	}
+    public int amount() {
+        throw new Error("unknown type: ${play.type}");
+    }
 
-	public int volumeCredit() {
-		int result = Math.max(performance.audience - 30, 0);
-		// add extra credit for every ten comedy attendees
-		return result;
-	}
+    public int volumeCredit() {
+        int result = Math.max(performance.audience - 30, 0);
+        return result;
+    }
 }
